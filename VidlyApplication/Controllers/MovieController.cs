65c07ed46a4ccdbc06f53347d26b324ecc4fea88
Anhle65 <levananh235813@gmail.com>
2023-12-19 14:51:12 +1300

    Replacing Vidly to VidlyApplication

---
+++ VidlyApplication/Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VidlyApplication.Models;
+
+namespace VidlyApplication.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            return View(movie);
+            //return Content(movie.Name);
+        }
+    }
+}
---
+++ VidlyApplication/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace VidlyApplication.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
